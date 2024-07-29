9484cce64c000076a7878cc50155a9b85176659d
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-07-29 15:59:14 +0800

    Update urls.py
    return to original before attempt to solve css loading issue

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -3,13 +3,6 @@ from django.urls import path, include
 from rest_framework import routers
 from restaurant import views
 from rest_framework.authtoken.views import obtain_auth_token
-from django.conf.urls.static import serve
-from .settings import get_mime_type
-
-import settings
-
-from django.conf.urls.static import static
-from .settings import DEBUG
 
 router = routers.DefaultRouter()
 # router.register(r'users', views.UsersView)
@@ -23,8 +16,4 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api-token-auth/', obtain_auth_token),
-    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT, 'mime_type': get_mime_type}),
 ]
-
-if DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
