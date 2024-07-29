9e15f106fb0d51b305131f33dd4b1e0e84dab31e
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-07-29 15:30:37 +0800

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -138,8 +138,6 @@ STATICFILES_DIRS = [
     "restaurant/static",
 ]
 
-STATIC_ROOT = "restaurant/static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
