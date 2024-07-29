512cee2ce6b40d480b5f717cbc7d795e4ec97ab6
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-07-29 15:42:25 +0800

    Update urls.py

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -3,6 +3,13 @@ from django.urls import path, include
 from rest_framework import routers
 from restaurant import views
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf.urls.static import serve
+from .settings import get_mime_type
+
+import settings
+
+from django.conf.urls.static import static
+from .settings import DEBUG
 
 router = routers.DefaultRouter()
 # router.register(r'users', views.UsersView)
@@ -16,4 +23,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api-token-auth/', obtain_auth_token),
+    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT, 'mime_type': get_mime_type}),
 ]
+
+if DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
