dc7d1bdf641edc021537fd8b543ad61be22168b2
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-07-29 15:52:11 +0800

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import dj_database_url
 import os
+import mimetypes
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
